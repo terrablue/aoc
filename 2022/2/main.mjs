cc9cd1026e6f851e29b36d0ac57a80e78bae3304
terrablue <102580937+terrablue@users.noreply.github.com>
2022-12-06 14:01:07 +0100

    2022 2

---
+++ 2022/2/main.mjs
@@ -0,0 +1,50 @@
+import {readFileSync} from "fs";
+
+const lines = readFileSync("real.input", {encoding: "utf-8"})
+  .split("\n")
+  .map(round => round.split(" "))
+  .slice(0, -1);
+
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+const lose = -1;
+const draw = 0;
+const win = +1; 
+
+const shapes = {
+  A: rock,
+  B: paper,
+  C: scissors,
+  X: rock,
+  Y: paper,
+  Z: scissors,
+};
+
+const shapesToSign = {
+  X: lose,
+  Y: draw,
+  Z: win
+};
+
+const mod = (a, m) => a >= 0
+  ? a % m
+  : (a + Math.ceil(-(a / m)) * m) % m;
+
+const roundScore1 = (opponent, me) => {
+  const opponentPlayed = shapes[opponent];
+  const mePlayed = shapes[me];
+  return [3, 6, 0][mod(mePlayed - opponentPlayed, 3)] + mePlayed + 1;
+};
+
+const roundScore2 = (opponent, me) => {
+  const opponentPlayed = shapes[opponent];
+  const mePlayed = mod(opponentPlayed + shapesToSign[me], 3);
+  return [3, 6, 0][mod(mePlayed - opponentPlayed, 3)] + mePlayed + 1;
+};
+
+const total = func => lines.reduce((score, [opponent, me]) =>
+  score + func(opponent, me), 0);
+
+console.log("part 1", total(roundScore1));
+console.log("part 2", total(roundScore2));
